1c3cd296bc6f4338cefc95b6c8de4ac86ef77585
FaizanFoomotion <faizan@foomotion.io>
2024-07-30 16:28:43 +0500

    user_article_association

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_30_065905) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_30_102208) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.time "created_at"
     t.time "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
